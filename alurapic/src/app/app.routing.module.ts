3421eca5575deee5a45fbb0cde2c81987112315a
Marcos Paulo <marcos.poo@gmail.com>
2024-07-13 08:11:16 -0300

    Estrutura inicial do projeto

---
+++ alurapic/src/app/app.routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { PhotoListComponent } from './photos/photo-list/photo-list.component';
+import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { PhotoListResover } from './photos/photo-list/photo-list.resover';
+
+const routes: Routes = [
+  {
+    path: 'user/:userName',
+    component: PhotoListComponent,
+    resolve: {
+      photos: PhotoListResover
+    }
+  },
+  {
+    path: 'p/add',
+    component: PhotoFormComponent
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
