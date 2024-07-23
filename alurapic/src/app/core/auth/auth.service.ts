18c3d21aa7ca2df13e91824bfdab6a6a5b56a929
Marcos Paulo <marcos.poo@gmail.com>
2024-07-22 22:55:15 -0300

    Aula 2.2 - Armazenamento do token

--- alurapic/src/app/core/auth/auth.service.ts
+++ alurapic/src/app/core/auth/auth.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { tap } from 'rxjs/operators';
 
+import { TokenService } from '../token/token.service';
+
 const API_URL = 'http://localhost:3000';
 
 @Injectable({
@@ -9,7 +11,10 @@ const API_URL = 'http://localhost:3000';
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private tokenService: TokenService
+  ) { }
 
   authenticate(userName: string, password: string){
 
@@ -21,6 +26,7 @@ export class AuthService {
       )
       .pipe(tap(res => {
         const authToken = res.headers.get('x-access-token');
+        this.tokenService.setToken(authToken);
         console.log(`User ${userName} authenticate with token ${authToken}`);
       }));
   }
