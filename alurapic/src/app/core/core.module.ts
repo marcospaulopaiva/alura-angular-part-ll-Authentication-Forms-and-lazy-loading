f76c175a64cb16457b2dd72c08d3306bae3c251f
Marcos Paulo <marcos.poo@gmail.com>
2024-07-24 20:05:17 -0300

    Aula 3.6 - A diretiva routerLink

--- alurapic/src/app/core/core.module.ts
+++ alurapic/src/app/core/core.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HeaderComponent } from './header/header.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-    imports: [
-      CommonModule
-    ],
-    declarations: [HeaderComponent],
-    exports: [HeaderComponent]
+  declarations: [HeaderComponent],
+  exports: [HeaderComponent]
+  imports: [
+      CommonModule,
+      RouterModule
+  ],
 })
 export class CoreModule { }
