765392aab1c27185aa65e7023056b3d278b24c7c
Marcos Paulo <marcos.poo@gmail.com>
2024-07-24 19:56:45 -0300

    Aula 3.5 - Guarda de rotas

--- alurapic/src/app/app.routing.module.ts
+++ alurapic/src/app/app.routing.module.ts
@@ -6,11 +6,13 @@ import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { PhotoListResover } from './photos/photo-list/photo-list.resover';
 import { SignInComponent } from './home/signin/signin.component';
+import { AuthGuard } from './core/auth/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
-    component: SignInComponent
+    component: SignInComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'user/:userName',
