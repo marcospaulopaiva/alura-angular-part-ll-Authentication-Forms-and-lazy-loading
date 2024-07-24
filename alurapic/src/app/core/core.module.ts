bce961533288349e58207701f96141075b913e2c
Marcos Paulo <marcos.poo@gmail.com>
2024-07-24 19:27:23 -0300

    Aula 3.3 - Async pipe

--- alurapic/src/app/core/core.module.ts
+++ alurapic/src/app/core/core.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
+    imports: [
+      CommonModule
+    ],
     declarations: [HeaderComponent],
     exports: [HeaderComponent]
 })
