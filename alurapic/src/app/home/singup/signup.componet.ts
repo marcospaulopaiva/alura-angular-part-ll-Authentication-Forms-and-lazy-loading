4a356f868a2561ad66181ff76e66c1e767472d6f
Marcos Paulo <marcos.poo@gmail.com>
2024-07-26 21:53:52 -0300

    Aula 4.4 - Validador assíncrono

--- alurapic/src/app/home/singup/signup.componet.ts
+++ alurapic/src/app/home/singup/signup.componet.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 import { lowerCaseValidator } from 'src/app/shared/validators/lower-case.validator';
+import { UserNotTakenValidatorService } from './user-not-taken.validator.service';
 
 @Component({
   templateUrl:'./signup.componet.html'
@@ -10,7 +11,10 @@ export class SignUpComponent implements OnInit{
 
   signupForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+      private formBuilder: FormBuilder,
+      private userNotTakenValidatorService: UserNotTakenValidatorService
+    ) {}
 
   ngOnInit(): void {
     this.signupForm = this.formBuilder.group({
@@ -33,7 +37,8 @@ export class SignUpComponent implements OnInit{
                 lowerCaseValidator,
                 Validators.minLength(2),
                 Validators.maxLength(30)
-            ]
+            ],
+            this.userNotTakenValidatorService.checkUserNameTaken()
         ],
         password: ['',
             [
