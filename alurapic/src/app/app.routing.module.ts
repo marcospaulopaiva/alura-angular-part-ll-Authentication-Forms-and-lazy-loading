ead420e447acdc75e656d6b6cdd101aa4096e811
Marcos Paulo <marcos.poo@gmail.com>
2024-07-27 21:40:08 -0300

    Aula 5.3 - Implementando o Lazy loading

--- alurapic/src/app/app.routing.module.ts
+++ alurapic/src/app/app.routing.module.ts
@@ -5,26 +5,17 @@ import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { PhotoListResover } from './photos/photo-list/photo-list.resover';
-import { AuthGuard } from './core/auth/auth.guard';
-import { SignInComponent } from './home/signin/signin.component';
-import { SignUpComponent } from './home/singup/signup.componet';
-import { HomeComponent } from './home/home.component';
+
 
 const routes: Routes = [
   {
-    path: '',
-    component: HomeComponent,
-    canActivate: [AuthGuard],
-    children: [
-      {
-          path: '',
-          component: SignInComponent,
-      },
-      {
-          path: 'signup',
-          component: SignUpComponent,
-      },
-  ]
+    path:'',
+    pathMatch:'full',
+    redirectTo:'home'
+  },
+  {
+    path: 'home',
+    loadChildren: './home/home.module#HomeModule'
   },
   {
     path: 'user/:userName',
---
+++ alurapic/src/app/home/home.routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+
+import { AuthGuard } from '../core/auth/auth.guard';
+import { HomeComponent } from './home.component';
+import { SignInComponent } from './signin/signin.component';
+import { SignUpComponent } from './singup/signup.componet';
+
+
+const routes: Routes = [
+  {
+      path: '',
+      component: HomeComponent,
+      canActivate: [AuthGuard],
+      children: [
+        {
+            path: '',
+            component: SignInComponent,
+        },
+        {
+            path: 'signup',
+            component: SignUpComponent,
+        },
+      ]
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
+})
+export class HomeRoutingModule {
+
+}
