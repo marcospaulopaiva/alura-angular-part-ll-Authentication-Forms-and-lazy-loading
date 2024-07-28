2a388ad12872a52be579c3094234e3d4b280940a
Marcos Paulo <marcos.poo@gmail.com>
2024-07-28 13:37:35 -0300

    Aula 5.5 - Interceptadores e envio do token

--- alurapic/src/app/core/core.module.ts
+++ alurapic/src/app/core/core.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
+import { RequestInterceptor } from './auth/request.interceptor';
 import { HeaderComponent } from './header/header.component';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
   declarations: [HeaderComponent],
@@ -11,5 +13,12 @@ import { RouterModule } from '@angular/router';
       CommonModule,
       RouterModule
   ],
+  providers: [
+      {
+        provide: HTTP_INTERCEPTORS,
+        useClass: RequestInterceptor,
+        multi: true
+      }
+  ]
 })
 export class CoreModule { }
